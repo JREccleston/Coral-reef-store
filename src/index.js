2aaededb9eda3d01cbace797e376140f6236694a
JREccleston <jreccleston@cox.net>
2023-11-07 11:24:46 -0600

    Added sidebar

--- src/index.js
+++ src/index.js
@@ -2,12 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { DefaultSidebar } from './componets/sidebar';
+import { ThemeProvider } from "@material-tailwind/react";
+
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 
-  <App />
+  <ThemeProvider>
+    <DefaultSidebar />
+  </ThemeProvider>
+
 
 );
 
