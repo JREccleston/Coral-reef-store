2842a2b88da332f18ec81feda2757f341900f7d7
JREccleston <jreccleston@cox.net>
2023-11-09 09:48:08 -0600

    updated coral desc

--- src/componets/strings.js
+++ src/componets/strings.js
@@ -1,4 +1,4 @@
 export const spsDesc = "A popular choice among aquarium enthusiasts due to their mesmerizing appearance. SPS corals require strong water flow, high-quality lighting, and excellent water conditions.";
-export const lpsDesc = "LPS corals are known for their vibrant colors and dynamic appearance, making them a fantastic addition to any coral reef aquarium";
+export const lpsDesc = "LPS corals are known for their vibrant coloration and dynamic appearance, making them a fantastic addition to any coral reef aquarium.";
 export const sftDesc = "Ideal for newcomers, soft corals make excellent additions to newly established reef tanks or serve as attractive accents in larger, well-established tanks."
 
