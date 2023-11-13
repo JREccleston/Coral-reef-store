c0bb5e0c33c531744e0790373fab716882cf4c71
JREccleston <jreccleston@cox.net>
2023-11-13 15:44:50 -0600

    Added refund policy

--- src/Pages/RefundPolicy.jsx
+++ src/Pages/RefundPolicy.jsx
@@ -2,8 +2,24 @@ import React from 'react'
 
 function RefundPolicy() {
     return (
-        <div>Poop</div>
+        <div className='bg-b2 w-full h-screen'>
+            <div className='inline text-center mx-auto font-sans'>
+                <h1 className='pt-20 text-6xl'>Refund Policy</h1>
+                <p className="w-1/2 mt-12 text-center mx-auto text-lg">
+                    All sales are final. Cancellations will be dealt with on a situational basis.
+                    We understand that emergencies can arise and will do our best to make it fair for both parties involved.
+                </p>
+
+                <p className="w-1/2 text-center mx-auto text-lg">
+                    In situations where a shipment is not delivered, or if the corals arrive partially or entirely deceased upon delivery, Thunder Corals may opt to provide a refund. However, in most instances, a store credit will be offered instead.
+                </p>
+                <p className="w-1/2 text-center mx-auto text-lg pt-6">
+                    This policy is implemented to assist Thunder Corals in mitigating losses incurred due to factors beyond their control, such as carrier delays or unforeseen acts of nature.
+                </p>
+            </div>
+        </div>
     )
 }
 
 export default RefundPolicy
+
--- src/Pages/TermsOfService.jsx
+++ src/Pages/TermsOfService.jsx
@@ -2,7 +2,12 @@ import React from 'react'
 
 function TermsOfService() {
     return (
-        <div>TermsOfService</div>
+        <div className='bg-b2'>
+            <p>
+                In situations where a shipment is not delivered, or if the corals arrive partially or entirely deceased upon delivery, Thunder Corals may opt to provide a refund. However, in most instances, a store credit will be offered instead.
+                This policy is implemented to assist Thunder Corals in mitigating losses incurred due to factors beyond their control, such as carrier delays or unforeseen acts of nature.
+            </p>
+        </div>
     )
 }
 
