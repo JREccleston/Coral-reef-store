55370616723414507911239b794cd78539c2c02a
JREccleston <jreccleston@cox.net>
2023-11-15 15:11:39 -0600

    Added navbar and footer to each product page

--- src/Pages/MotherColPage.jsx
+++ src/Pages/MotherColPage.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
+import { NavbarWithMegaMenu } from '../componets/NavBarWithMegaMenu'
+import { SimpleFooter } from '../componets/Footer'
+
 
 function MotherColPage() {
     return (
-        <div>MotherColPage</div>
+        <div className='bg-b2 w-full h-screen'>
+            <div>
+                <NavbarWithMegaMenu />
+            </div>
+            <div className="sticky top-[100vh]">
+                <SimpleFooter />
+            </div>
+        </div>
     )
 }
 
