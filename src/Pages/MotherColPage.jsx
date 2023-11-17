1420d6851da8ce69cc25378d4a57ed368947569d
JREccleston <jreccleston@cox.net>
2023-11-17 12:07:10 -0600

    Updating sig col page

--- src/Pages/MotherColPage.jsx
+++ src/Pages/MotherColPage.jsx
@@ -9,6 +9,17 @@ function MotherColPage() {
             <div className='pb-40'>
                 <NavbarWithMegaMenu />
             </div>
+            <div>
+                <h1 className='text-5xl text-center pb-3'>
+                    Signiture Colonies
+                </h1>
+                <p className='text-lg text-center'>
+                    Here is a collection of my longtime favorite colonies currently in my display tank
+                </p>
+                <p className='text-lg text-center'>
+                    If you see something you like, don't hesitate to reach out
+                </p>
+            </div>
             <div className="sticky top-[100vh]">
                 <SimpleFooter />
             </div>
