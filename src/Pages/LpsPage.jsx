71f1275a45cbe097fca78160870cfb5bd7805af6
JREccleston <jreccleston@cox.net>
2023-11-29 09:52:13 -0600

    Updated product card spacing for lps page

--- src/Pages/LpsPage.jsx
+++ src/Pages/LpsPage.jsx
@@ -23,7 +23,7 @@ function LpsPage() {
             <div>
 
             </div>
-            <div className='flex flex-row flex-wrap items-center justify-center gap-x-24 gap-y-20 pb-20' dangerouslySetInnerHTML={{ __html: lpsProductContainer }}>
+            <div className='flex flex-row flex-wrap items-center justify-center gap-x-28 gap-y-20 pb-20' dangerouslySetInnerHTML={{ __html: lpsProductContainer }}>
 
             </div>
             <div className="sticky top-[100vh]">
